adfbda79930797ae5c95605e9cf7ce603d3a7347
Emil Malmsten <emil_malmsten@hotmail.com>
2023-12-24 15:05:38 +0700

    fix: nullable types

--- User.cs
+++ User.cs
@@ -1,6 +1,6 @@
 public class User : BaseEntity
 {
     public required string Name { get; set; }
-    public string ApiKey { get; set; }
-    public List<Feed> Feeds { get; set; }
+    public string? ApiKey { get; set; }
+    public List<Feed>? Feeds { get; set; }
 }
